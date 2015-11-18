0cadbc09c1db63a1d08484e4550829e3b7f88098
parland <parland@mail.ru>
2015-11-18 18:20:17 +0200

    initial div generation

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 /**
  * Created by andrej on 18/11/15.
  */
+$(document).ready(function () {
+
+    function newShutter(paramX, paramY) {
+        var newDiv = $('<div>').addClass("shutter");
+        return newDiv.css({top: paramY + "px", left: paramX + "px"});
+    }
+
+
+    for (var i = 0; i < 30; i++) {
+        var x = Math.random() * 30 - Math.random() * 15;
+        var y = Math.random() * 30 - Math.random() * 15;
+        $(".container").append(newShutter(x, y));
+    }
+});
