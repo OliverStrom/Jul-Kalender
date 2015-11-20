18d9dfaeb6617fd9d6ec01494953dad97abd0324
parland <parland@mail.ru>
2015-11-20 17:14:03 +0200

    drops

--- script.js
+++ script.js
@@ -4,28 +4,35 @@
 $(document).ready(function () {
 
     function newShutter(paramX, paramY) {
-        var newDiv = $('<div>').addClass("shutter");
+        var newDiv = $('<div>').addClass("shutter").click(drop);
         return newDiv.css({top: paramY + "px", left: paramX + "px"});
     }
 
-    function shuffle(o){
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    function shuffle(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         return o;
+    }
+
+    function rotateRandom(obj) {
+        return obj.css({transform: "rotate(" + 90 * Math.floor(Math.random() * 4) + "deg)"});
+    }
+
+    function drop() {
+        return $(this).animate({top:'2000px'},'slow','swing');
     }
 
     function createRandomDivs() {
         var dates = [];
-        for (var i = 0; i < 24; i++) {
-            dates.push(i + 1);
+        for (var j = 0; j < 24; j++) {
+            dates.push(j + 1);
         }
         shuffle(dates);
-
-
         for (var i = 0; i < 24; i++) {
             var x = Math.random() * 30 - Math.random() * 15;
             var y = Math.random() * 30 - Math.random() * 15;
-            $(".container").append(newShutter(x, y).html(dates[i] + 1));
+            $(".container").append(rotateRandom(newShutter(x, y).html(dates[i])));
         }
     }
+
     createRandomDivs();
 });
