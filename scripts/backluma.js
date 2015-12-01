19c53e9fc3acb9f6acc87ae6ff235c8c8331f4dc
Newbcake123 <Djurcell69>
2015-12-01 12:20:17 +0200

    Backluma

--- scripts/backluma.js
+++ scripts/backluma.js
@@ -134,6 +134,7 @@ function backluma_main() {
 			// Score
 			ctx.fillStyle = "rgb(0, 0, 0)";
 			ctx.font = "24px Helvetica";
+			ctx.marginTop = "50px";
 			ctx.fillText("Presents retrieved: " + monstersCaught, 32, 32);
 		};
 		
