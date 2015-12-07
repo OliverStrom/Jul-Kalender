0324d21cee5a05fe9a84d1fb2f19c72d25b9fb3f
gronroga <gronroga@arcada.fi>
2015-12-07 13:34:51 +0200

    Test, jippii

--- scripts/gronroga.js
+++ scripts/gronroga.js
@@ -1,61 +1,70 @@
 /**
  * Created by parland on 11/21/15.
  */
-function gronroga_main() {
-    //alert($(this).html());
 
-    // skapa new DIV med klass "closable"
-    var newDiv = $("<div>").attr("class", "closable");
-    // lägg till den nya DIVen till kontainern
-    $("#container").append(newDiv);
-    // formatera
-    newDiv.css({
-        position: "absolute",
-        top: 150,
-		left:600,
-		right: 0,
-		bottom: 0,
-        width: "450px",
-        height: "450px",
-        color: "white",
-        backgroundColor: "darkred",
-        padding: "20px",
-		backgroundImage: "url('assets/pictures/fatherski.gif')",
-		backgroundRepeat: "no-repeat",
-		backgroundPosition: 'center center',
-		
-    });
+function gronroga_main (){
 
-    // fylla på innehållet
-    newDiv.html("Detta är Gabriellas lucka för den " + $(this).html() + " december.<br><br>");
-   /* newDiv.append("Nu så kommer julen, <br>" +
-					"nu är julen här,<br>" +
-					"lite mörk och kulen<br>" +
-					"men ändå så kär.<br>" +
-					"Han i salen träder<br>" +
-					"med så hjärtligt sinn<br>" +
-					"och i högtidskläder<br>" +
-					"dansar barnen in.<br>" +
-					"Ljusen i lanternan<br>" +
-					"glimmar högt kring dem,<br>" +
-					"liksom klara stjärnan<br>" +
-					"över Betlehem.<br>" +
-					"Zacharias Topelius<br>");*/
-	
-	// EGET 
-	
-	
 
-	//EGET SLUT
-    function closeDiv() {
-        $(".closable").remove();
+    function run() {
+        var image = document.getElementById('background');
+        image.onload = function() {
+            console.log(this);
+            var engine = new RainyDay({
+                image: this,
+                parentElement: document.getElementById('test'),
+                top: 0,
+                left: 0
+            });
+            engine.rain([ [1, 2, 8000] ]);
+            engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
+        };
+        image.crossOrigin = 'anonymous';
+        image.src = 'assets/pictures/fatherski.gif';
     }
 
-    // koppla till avstägningsfunktion
-    $("body").keydown(function (e) {
-        if (e.which === 27)closeDiv();
-    });
 
-    // skapa avstägningsknapp
-    newDiv.append($("<div>").attr("id", "close").html("X").click(closeDiv));
+    function getDivStructure(){
+        return $('<div>')
+            .css({
+                opacity:"0"
+            })
+            .animate({
+                opacity:"1"
+            }, 1000)
+            .append($('<div id="test">')
+                .append($('<img id="background"  alt="background" src="" crossorigin="anonymous">')
+                    .css({
+                        left: "0px",
+                        top: "0px",
+                        position: "absolute",
+                        height:"460",
+                        width:"560"
+            })))
+            .append($('<div>')
+                .css({
+                        position:"absolute",
+                        fontSize:"40px",
+                        fontWeight: "600",
+                        zOrder: "10",
+                        left:"120px",
+                        top: "30px",
+						color:"rgba(255, 255, 255, 1)",
+						fontFamily:"Indie Flower"
+                    })
+                .append("SE UPP I BACKEN!")
+            )
+    }
+    $(this) 
+        .parent() 
+        .append( 
+            window.ClosableDiv.init(getDivStructure()) 
+                .css({
+                    height:"420",
+                    width:"520",
+                    backgroundColor:"rgba(0, 0, 50, 0.1)" 
+                }) 
+        );
+
+    run();
+
 }
