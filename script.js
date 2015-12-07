bd0838624f40d8c8193feeeb807716964f03f9d8
Westeann <alexandrawesterback@me.com>
2015-12-07 14:08:22 +0200

    sry

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
         "stromjoe",
         "stromoli",
         "westeann",
-		"westeann-2"
+		"westeann-2",
         "persona_1",
         "persona_2",
         "persona_3",
