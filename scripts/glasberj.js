d7204963e471364befe426ac2249fd12620ba06f
glasberj <julia.glasberg@gmail.com>
2015-12-07 14:04:19 +0200

    hej

--- scripts/glasberj.js
+++ scripts/glasberj.js
@@ -1,46 +1,70 @@
 /**
  * Created by parland on 11/21/15.
  */
-function glasberj_main() {
-    //alert($(this).html());
 
-    // skapa new DIV med klass "closable"
-    var newDiv = $("<div>").attr("class", "closable");
-    // lägg till den nya DIVen till kontainern
-    $("#container").append(newDiv);
-    // formatera
-    newDiv.css({
-        position: "absolute",
-        top: "100px",
-        left: "300px",
-        width: "1000px",
-        height: "500px",
-        color: "#EDBE26",
-		fontSize : "20px",
-		backgroundColor : "#000000",
-		backgroundImage: 'url("assets/pictures/santa.gif")',
-		backgroundRepeat: 'no-repeat',
-		backgroundPosition: 'center center',
-        padding: "20px"
-    });
+function glasberj_main (){
 
-    // fylla på innehållet
-    newDiv.html($(this).html() + " december.<br><br>");
-    newDiv.append("En röd, glad liten gubbe, <br>" +
-        "vad kan det vara för snubbe.<br>" +
-        "Kommer till dig när du varit snäll,<br>" +
-		"och inte på ett år hört något gnäll.");
 
-    function closeDiv() {
-        $(".closable").remove();
+    function run() {
+        var image = document.getElementById('background');
+        image.onload = function() {
+            console.log(this);
+            var engine = new RainyDay({
+                image: this,
+                parentElement: document.getElementById('test'),
+                top: 0,
+                left: 0
+            });
+            engine.rain([ [1, 2, 8000] ]);
+            engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
+        };
+        image.crossOrigin = 'anonymous';
+        image.src = 'assets/pictures/winter.jpg';
     }
 
-    // koppla till avstägningsfunktion
-    $("body").keydown(function (e) {
-        if (e.which === 27)closeDiv();
-    });
 
-    // skapa avstägningsknapp
-    newDiv.append($("<div>").attr("id", "close").html("X").click(closeDiv));
-	
+    function getDivStructure(){
+        return $('<div>')
+            .css({
+                opacity:"0"
+            })
+            .animate({
+                opacity:"1"
+            }, 1000)
+            .append($('<div id="test">')
+                .append($('<img id="background"  alt="background" src="" crossorigin="anonymous">')
+                    .css({
+                        left: "0px",
+                        top: "0px",
+                        position: "absolute",
+                        height:"500",
+                        width:"840"
+            })))
+            .append($('<div>')
+                .css({
+                        position:"absolute",
+                        fontSize:"90px",
+                        fontWeight: "600",
+						fontFamily: "Lobster",
+                        zOrder: "10",
+						color: "black",
+						marginTop: "50px"
+                    })
+                .append("<img src='assets/pictures/lights.gif'>")
+				.append("God Jul(ia)!<br>")
+            )
+    }
+    $(this) 
+        .parent() 
+        .append( 
+            window.ClosableDiv.init(getDivStructure()) 
+                .css({
+                    height:"420",
+                    width:"520",
+                    backgroundColor:"rgba(0, 0, 50, 0.1)"
+                }) 
+        );
+
+    run();
+
 }
